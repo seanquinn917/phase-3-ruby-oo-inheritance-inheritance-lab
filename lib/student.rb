393a3af4bb1a5b6b6461774a70c9b0be3996d45c
SeanQuinn <Seanquinn917@gmail.com>
2023-09-19 15:03:14 -0400

    commit tree df1cda1d7be6f417d5db26dbb516b5dd7734e50a

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+class Student < User
+
+attr_accessor :knowledge
+
+
+
+
+def initialize(knowledge=[])
+    @knowledge = knowledge
+    
+end
+
+def learn(string)
+@knowledge << string
+end
+
+def knowledge
+    @knowledge
+end
 
 end
